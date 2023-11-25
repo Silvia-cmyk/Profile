9b6ac6dc922559f4aa349a6d9cb456258d34360b
Silvia-cmyk <seaforest01225@gmail.com>
2023-11-26 02:40:23 +0800

    Add content with touch screen flexible(Because the script (JS) wasn't included, it caused the problem to occur.)

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.querySelector('#sideNav');
+    const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
-        new bootstrap.ScrollSpy(sideNav, {
-            target: '#navbarResponsive',
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
         });
-    }
+    };
 
     // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.querySelector('.navbar-toggler');
-    const responsiveNavItems = document.querySelectorAll('#navbarResponsive .nav-link');
-
-    responsiveNavItems.forEach(responsiveNavItem => {
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
